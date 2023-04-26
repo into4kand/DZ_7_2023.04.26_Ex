8fef37b7ffdd2e618459fa2726996acec0bbe940
Inna Glebova <into4kand@mail.ru>
2023-04-27 00:22:18 +0300

    Third Ex done

---
+++ Ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+Console.WriteLine("Введите число для проверки на чётность ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number % 2;
+if (a == 0)
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}

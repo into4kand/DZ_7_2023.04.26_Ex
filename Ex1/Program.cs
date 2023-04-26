a714b40422d079fd00a63895442579bde0fb1400
Inna Glebova <into4kand@mail.ru>
2023-04-26 23:31:04 +0300

    First Ex done

---
+++ Ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("Максимальное число " + a);
+    Console.WriteLine("Минимальное число " + b);
+}
+else if (a < b)
+{
+    Console.WriteLine("Максимальное число " + b);
+    Console.WriteLine("Минимальное число " + a);
+}
+else
+{
+    Console.WriteLine("Максимальное и минимальное число в ряду это " + a);
+}

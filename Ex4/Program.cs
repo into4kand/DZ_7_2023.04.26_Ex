8c6895ef36e49935b139f389d9b0e712432d4164
Inna Glebova <into4kand@mail.ru>
2023-04-27 00:37:16 +0300

    Fourth Ex done

---
+++ Ex4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 2;
+
+if (N > 1)
+{
+    Console.Write("Четные числа ");
+        while (a <= N)
+    {
+        Console.Write(a + " ");
+        a = a + 2;
+    }
+}
